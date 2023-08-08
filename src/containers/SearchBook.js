0f70644b0d39fe801d05cac5ade015681ed397d7
samuel <timeuss.craft@gmail.com>
2023-08-08 15:19:14 +0200

    add search page

--- src/containers/SearchBook.js
+++ src/containers/SearchBook.js
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SearchBook = () => {
-  return <div>Fetch & SearchBook</div>
+  const [title, setTitle] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title)
+  }
+
+  return (
+    <main role="main">
+      <div className="jumbotron jumbotron-fluid mb-5 ">
+        <div className="container text-center pb-3">
+          <h1 className="display-4">BOOKS</h1>
+          <p>Indiquez le sujet du livre que vous voulez ajouter</p>
+          <form
+            onSubmit={handleSubmit}
+            className="form-inline d-flex justify-content-center"
+          >
+            <div className="from-group">
+              <input
+                required
+                type="text"
+                className="form-control"
+                placeholder="Quoi rechercher ?"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+
+            <div className="from-group">
+              <button className="btn btn-outline-secondary ms-3">
+                Rechercher
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="container" style={{ minHeight: '200px' }}>
+        <div className="accordion">
+          <div className="card mb-2">
+            <div className="card-header"></div>
+            <div className="collapse" data-parent="accordion">
+              <div className="card-body"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default SearchBook
